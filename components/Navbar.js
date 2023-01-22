88fbd8456da2678e71ed09c3f7709740f86a63f2
alexandre <alexandrechabane72@gmail.com>
2023-01-22 23:53:11 +0100

    modify navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,6 +16,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { userService } from "@/services";
+import { Link } from './Link';
 
 const solutions = [
   {
@@ -90,14 +91,14 @@ export default function Example() {
       <div className="mx-auto max-w-7xl px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
+            <Link href="/">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto sm:h-10"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
               />
-            </a>
+            </Link>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -138,7 +139,7 @@ export default function Example() {
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                           {solutions.map((item) => (
-                            <a
+                            <Link
                               key={item.name}
                               href={item.href}
                               className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
@@ -148,19 +149,19 @@ export default function Example() {
                                 <p className="text-base font-medium text-gray-900">{item.name}</p>
                                 <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                               </div>
-                            </a>
+                            </Link>
                           ))}
                         </div>
                         <div className="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
                           {callsToAction.map((item) => (
                             <div key={item.name} className="flow-root">
-                              <a
+                              <Link
                                 href={item.href}
                                 className="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100"
                               >
                                 <item.icon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
                                 <span className="ml-3">{item.name}</span>
-                              </a>
+                              </Link>
                             </div>
                           ))}
                         </div>
@@ -171,12 +172,12 @@ export default function Example() {
               )}
             </Popover>
 
-            <a href="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <Link href="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Contact
-            </a>
-            <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            </Link>
+            <Link href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
               About
-            </a>
+            </Link>
 
             <Popover className="relative">
               {({ open }) => (
@@ -210,7 +211,7 @@ export default function Example() {
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                           {resources.map((item) => (
-                            <a
+                            <Link
                               key={item.name}
                               href={item.href}
                               className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
@@ -220,7 +221,7 @@ export default function Example() {
                                 <p className="text-base font-medium text-gray-900">{item.name}</p>
                                 <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                               </div>
-                            </a>
+                            </Link>
                           ))}
                         </div>
                         <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
@@ -229,18 +230,18 @@ export default function Example() {
                             <ul role="list" className="mt-4 space-y-4">
                               {recentPosts.map((post) => (
                                 <li key={post.id} className="truncate text-base">
-                                  <a href={post.href} className="font-medium text-gray-900 hover:text-gray-700">
+                                  <Link href={post.href} className="font-medium text-gray-900 hover:text-gray-700">
                                     {post.name}
-                                  </a>
+                                  </Link>
                                 </li>
                               ))}
                             </ul>
                           </div>
                           <div className="mt-5 text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            <Link href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
                               View all posts
                               <span aria-hidden="true"> &rarr;</span>
-                            </a>
+                            </Link>
                           </div>
                         </div>
                       </div>
@@ -291,44 +292,44 @@ export default function Example() {
               <div className="mt-6">
                 <nav className="grid gap-y-8">
                   {solutions.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
                       className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
                     >
                       <item.icon className="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
                       <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                    </a>
+                    </Link>
                   ))}
                 </nav>
               </div>
             </div>
             <div className="space-y-6 py-6 px-5">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="/contact" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <Link href="/contact" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Contact
-                </a>
+                </Link>
 
-                <a href="/about" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <Link href="/about" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   About
-                </a>
+                </Link>
                 {resources.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div>
-                <a
+                <button
                   onClick={logout}
                   className="flex w-full items-center justify-center rounded-md border border-transparent bg-welmo-blue px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-welmo-blue-hover"
                 >
                   Se déconnecter
-                </a>
+                </button>
               </div>
             </div>
           </div>
