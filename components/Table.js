b5cda53aeecb7322efa2fb1dfff593f62eac9487
alexandre <alexandrechabane72@gmail.com>
2023-02-01 00:19:38 +0100

    add classname

--- components/Table.js
+++ components/Table.js
@@ -11,31 +11,31 @@ const Table = () => {
   return (
     <>
       <div className="mx-1 md:mx-6">
-        <table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5 sm:inline-table">
-          <thead class="text-white">
+        <table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5 sm:inline-table">
+          <thead className="text-white">
             {users.map((listValue, index) => {
               return (
-                <tr class="bg-welmo-blue flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0 [&:not(:first-child)]:sm:hidden">
-                  <th class="p-3 text-left">Name</th>
-                  <th class="p-3 text-left">Email</th>
-                  <th class="p-3 text-left" width="110px">
+                <tr className="bg-welmo-blue flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0 [&:not(:first-child)]:sm:hidden">
+                  <th className="p-3 text-left">Name</th>
+                  <th className="p-3 text-left">Email</th>
+                  <th className="p-3 text-left" width="110px">
                     Actions
                   </th>
                 </tr>
               );
             })}
           </thead>
-          <tbody class="flex-1 sm:flex-none">
+          <tbody className="flex-1 sm:flex-none">
             {users.map((listValue, index) => {
               return (
-                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 hover:bg-gray-100">
-                  <td class="border-grey-light border p-3">
+                <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 hover:bg-gray-100">
+                  <td className="border-grey-light border p-3">
                     {listValue.firstName} {listValue.lastName}
                   </td>
-                  <td class="border-grey-light border p-3 truncate">
+                  <td className="border-grey-light border p-3 truncate">
                     {listValue.username}
                   </td>
-                  <td class="border-grey-light border p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
+                  <td className="border-grey-light border p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">
                     Delete
                   </td>
                 </tr>
