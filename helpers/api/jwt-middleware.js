242be63937c56b8cd6bed45e3ab7e680d8959274
alexandre <alexandrechabane72@gmail.com>
2023-02-01 01:00:54 +0100

    delete verif jwt

--- helpers/api/jwt-middleware.js
+++ helpers/api/jwt-middleware.js
@@ -1,22 +1,22 @@
-const { expressjwt: expressJwt } = require('express-jwt');
-const util = require('util');
-import getConfig from 'next/config';
+const { expressjwt: expressJwt } = require("express-jwt");
+const util = require("util");
+import getConfig from "next/config";
 
 const { serverRuntimeConfig } = getConfig();
 
 export { jwtMiddleware };
 
 function jwtMiddleware(req, res) {
-    console.log('azertyuiop')
-    const middleware = expressJwt({ secret: serverRuntimeConfig.secret, algorithms: ['HS256'] }).unless({
-        path: [
-            // public routes that don't require authentication
-            '/api/users/register',
-            '/api/users/authenticate'
-        ]
-    });
-    console.log('middleware')
-    console.log(middleware)
+  const middleware = expressJwt({
+    secret: serverRuntimeConfig.secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+      // public routes that don't require authentication
+      "/api/users/register",
+      "/api/users/authenticate",
+    ],
+  });
 
-    return util.promisify(middleware)(req, res);
+  return util.promisify(middleware)(req, res);
 }
