5d7fb4994bfef776646fafca7d071fe5384a418f
alexandre <alexandrechabane72@gmail.com>
2023-02-05 01:51:37 +0100

    add classname

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import Navbar from "@/components/Navbar";
 const Home = (props) => {
   return (
     <>
-      <div class="isolate bg-white">
-        <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+      <div classname="isolate bg-white">
+        <div classname="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
           <svg
-            class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+            classname="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
             viewBox="0 0 1155 678"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -31,75 +31,75 @@ const Home = (props) => {
           </svg>
         </div>
         <Navbar />
-        <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap -m-4 text-center">
-              <div class="p-4 sm:w-1/4 w-1/2">
-                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+        <section classname="text-gray-600 body-font">
+          <div classname="container px-5 py-24 mx-auto">
+            <div classname="flex flex-wrap -m-4 text-center">
+              <div classname="p-4 sm:w-1/4 w-1/2">
+                <h2 classname="title-font font-medium sm:text-4xl text-3xl text-gray-900">
                   2.7K
                 </h2>
-                <p class="leading-relaxed">Users</p>
+                <p classname="leading-relaxed">Users</p>
               </div>
-              <div class="p-4 sm:w-1/4 w-1/2">
-                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+              <div classname="p-4 sm:w-1/4 w-1/2">
+                <h2 classname="title-font font-medium sm:text-4xl text-3xl text-gray-900">
                   1.8K
                 </h2>
-                <p class="leading-relaxed">Subscribes</p>
+                <p classname="leading-relaxed">Subscribes</p>
               </div>
-              <div class="p-4 sm:w-1/4 w-1/2">
-                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+              <div classname="p-4 sm:w-1/4 w-1/2">
+                <h2 classname="title-font font-medium sm:text-4xl text-3xl text-gray-900">
                   35
                 </h2>
-                <p class="leading-relaxed">Downloads</p>
+                <p classname="leading-relaxed">Downloads</p>
               </div>
-              <div class="p-4 sm:w-1/4 w-1/2">
-                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+              <div classname="p-4 sm:w-1/4 w-1/2">
+                <h2 classname="title-font font-medium sm:text-4xl text-3xl text-gray-900">
                   4
                 </h2>
-                <p class="leading-relaxed">Products</p>
+                <p classname="leading-relaxed">Products</p>
               </div>
             </div>
           </div>
         </section>
-        <div class="relative px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
-            <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+        <div classname="relative px-6 lg:px-8">
+          <div classname="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
+            <div classname="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div classname="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Announcing our next round of funding.{" "}
-                <a href="#" class="font-semibold text-indigo-600">
-                  <span class="absolute inset-0" aria-hidden="true"></span>Read
-                  more <span aria-hidden="true">&rarr;</span>
+                <a href="#" classname="font-semibold text-indigo-600">
+                  <span classname="absolute inset-0" aria-hidden="true"></span>
+                  Read more <span aria-hidden="true">&rarr;</span>
                 </a>
               </div>
             </div>
-            <div class="text-center">
-              <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <div classname="text-center">
+              <h1 classname="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                 Data to enrich your online business
               </h1>
-              <p class="mt-6 text-lg leading-8 text-gray-600">
+              <p classname="mt-6 text-lg leading-8 text-gray-600">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
                 lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
                 fugiat aliqua.
               </p>
-              <div class="mt-10 flex items-center justify-center gap-x-6">
+              <div classname="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
-                  class="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  classname="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
                 </a>
                 <a
                   href="#"
-                  class="text-base font-semibold leading-7 text-gray-900"
+                  classname="text-base font-semibold leading-7 text-gray-900"
                 >
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
           </div>
-          <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]">
+          <div classname="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]">
             <svg
-              class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+              classname="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
               viewBox="0 0 1155 678"
               xmlns="http://www.w3.org/2000/svg"
             >
